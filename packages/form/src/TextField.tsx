054550f474186d5ad0412067a60ff6fe200f7978
Bryan Overbey <bryanmorganoverbey@gmail.com>
2022-08-01 16:19:19 -0400

    fix css

--- packages/form/src/TextField.tsx
+++ packages/form/src/TextField.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {  styled, ThemeProvider} from "@mui/material/styles";
+import { ThemeProvider} from "@mui/material/styles";
 import { TextField as MUITextField, InputLabel } from '@mui/material';
 import { useField } from 'formik';
 import { v4 as uuid } from "uuid";
@@ -10,33 +10,8 @@ const TextField = ({ tag: Tag, className, onChange: propsOnChange, labelText, va
   const [{ onChange: useFieldOnChange, ...field }, metadata, helpers] = useField(
     name
   );
-  console.log('field', field)
-  console.log('metadata', metadata)
 
-  // const DJTextField = styled(MUITextField)(({ theme }) => ({
-  //   marginTop: "16px",
-  //   // "& .MuiInputBase-input": {
-  //   //   fontSize: 16,
-  //   //   fontWeight: 400,
-  //   //   width: "auto",
-  //   //   padding: "16px 12px 16px 12px",
-  //   //   "&:focus": {
-  //   //     borderColor: theme.palette.primary.main,
-  //   //   },
-  //   //   border: `1px solid`,
-  //   //   borderRadius: "3px",
-  //   //   boxSizing: "border-box",
-  //   //   height: "48px",
-  //   // },
-  //   // "& .MuiFormHelperText-root": {
-  //   //   color: "#373F50",
-  //   //   fontSize: '12px',
-  //   //   fontWeight: 400,
-  //   // },
-  //   // "& .MuiFormHelperText-root.Mui-error": {
-  //   //   color: theme.palette.error.main
-  //   // },
-  // }));
+
   const htmlId = rest?.id || uuid();
   const inputLabelStyle = {
     color: metadata.error
@@ -45,17 +20,14 @@ const TextField = ({ tag: Tag, className, onChange: propsOnChange, labelText, va
   };
   const rootStyle = {
         marginTop: "8px",
-        // color: theme.palette.metalgrey.main,
-        // fontSize: 16,
-        // fontWeight: 400,
-        // width: "auto",
+        fontSize: "16px",
+        fontWeight: 400,
+        width: "auto",
         padding: "8px 6px",
         "&:focus": {
           borderColor: theme.palette.primary.main,
         },
-        // border: `1px solid`,
-        // borderRadius: "3px",
-        // // boxSizing: "border-box",
+        boxSizing: "border-box",
         color: metadata.error? theme.palette.error.main : "#56627C",
         height: "48px",
   };
@@ -69,8 +41,9 @@ const TextField = ({ tag: Tag, className, onChange: propsOnChange, labelText, va
         id={htmlId}
         variant="outlined"
         {...rest}
-        InputLabelProps={{ shrink: false, style: { color: "#56627C" } }}
+        InputLabelProps={{ shrink: false, style: { color: "#56627C" , padding: "0px 5px"} }}
         label={!metadata.touched && !field.value && label}
+        
         className={className}
         name={name}
         required={required}
@@ -78,7 +51,7 @@ const TextField = ({ tag: Tag, className, onChange: propsOnChange, labelText, va
         error={!!metadata.error}
         helperText={metadata.error || helpMessage}
         InputProps={{
-          style: { color: metadata.error ? theme.palette.error.main : "#56627C", }
+          style: { color: metadata.error ? theme.palette.error.main : "#56627C" }
         }}
         FormHelperTextProps={{
           style: {
