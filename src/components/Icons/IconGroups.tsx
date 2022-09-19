1c665d899107bbbe613c1150c4a9c86feaef2502
Bryan Overbey <bryanmorganoverbey@gmail.com>
2022-09-19 11:20:09 -0400

    add more icons

---
+++ src/components/Icons/IconGroups.tsx
@@ -0,0 +1,31 @@
+import * as React from "react";
+import { SVGProps } from "react";
+interface SVGRProps {
+  title?: string;
+  titleId?: string;
+}
+
+const SvgIconGroups = ({
+  title,
+  titleId,
+  ...props
+}: SVGProps<SVGSVGElement> & SVGRProps) => (
+  <svg
+    viewBox="0 0 24 24"
+    fill="currentColor"
+    xmlns="http://www.w3.org/2000/svg"
+    aria-labelledby={titleId}
+    {...props}
+  >
+    {title ? <title id={titleId}>{title}</title> : null}
+    <path
+      d="M20.65 21v-.933c0-.495-.211-.97-.586-1.32a2.075 2.075 0 0 0-1.415-.547h-4c-.53 0-1.039.197-1.414.547-.375.35-.586.825-.586 1.32V21M16.65 17a2 2 0 1 0 0-4 2 2 0 0 0 0 4ZM6 10a2 2 0 1 0 0-4 2 2 0 0 0 0 4ZM18 10a2 2 0 1 0 0-4 2 2 0 0 0 0 4ZM11 21v-.933c0-.495-.21-.97-.586-1.32A2.075 2.075 0 0 0 9 18.2H5c-.53 0-1.04.197-1.414.547-.375.35-.586.825-.586 1.32V21M7 17a2 2 0 1 0 0-4 2 2 0 0 0 0 4ZM12 13a2 2 0 1 0 0-4 2 2 0 0 0 0 4ZM12 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4Z"
+      stroke="currentColor"
+      strokeWidth={1.143}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+
+export default SvgIconGroups;
---
+++ src/components/Icons/IconUsers.tsx
@@ -0,0 +1,31 @@
+import * as React from "react";
+import { SVGProps } from "react";
+interface SVGRProps {
+  title?: string;
+  titleId?: string;
+}
+
+const SvgIconUsers = ({
+  title,
+  titleId,
+  ...props
+}: SVGProps<SVGSVGElement> & SVGRProps) => (
+  <svg
+    viewBox="0 0 24 24"
+    fill="currentColor"
+    xmlns="http://www.w3.org/2000/svg"
+    aria-labelledby={titleId}
+    {...props}
+  >
+    {title ? <title id={titleId}>{title}</title> : null}
+    <path
+      d="M21.782 14.12v-1.064a2.02 2.02 0 0 0-.697-1.505 2.529 2.529 0 0 0-1.682-.623h-4.758c-.631 0-1.236.224-1.682.623a2.02 2.02 0 0 0-.697 1.505v1.064M17.024 9.56c1.314 0 2.379-1.02 2.379-2.28 0-1.26-1.065-2.28-2.38-2.28-1.313 0-2.378 1.02-2.378 2.28 0 1.26 1.065 2.28 2.379 2.28ZM12.517 19.44v-1.064c0-.565-.251-1.106-.697-1.505a2.529 2.529 0 0 0-1.683-.623H5.38c-.63 0-1.236.224-1.682.623-.446.4-.697.94-.697 1.505v1.064M7.758 14.88c1.314 0 2.38-1.02 2.38-2.28 0-1.26-1.066-2.28-2.38-2.28-1.314 0-2.38 1.02-2.38 2.28 0 1.26 1.066 2.28 2.38 2.28Z"
+      stroke="currentColor"
+      strokeWidth={1.303}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+
+export default SvgIconUsers;
